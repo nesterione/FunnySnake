357b8a6a11335a8c55c7a81b71468b88c78a90cb
Igor Nesterenya <nesterione@gmail.com>
2014-07-23 00:03:30 +0300

    Init libGdx project

---
+++ desktop/src/com/nesterenya/fannysnake/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.nesterenya.fannysnake.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.nesterenya.fannysnake.FannySnake;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new FannySnake(), config);
+	}
+}
