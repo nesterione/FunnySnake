b4793b6e1212968b2aeccee0ca14448f92980aa5
Igor Nesterenya <nesterione@gmail.com>
2014-08-20 18:55:34 +0300

    Add support android control

--- core/src/com/nesterenya/fannysnake/control/MobileControl.java
+++ core/src/com/nesterenya/fannysnake/control/MobileControl.java
@@ -1,23 +1,38 @@
 package com.nesterenya.fannysnake.control;
 
-public class MobileControl  implements MotionControl{
+import com.badlogic.gdx.Gdx;
+import com.nesterenya.fannysnake.GameContext;
 
+public class MobileControl  implements MotionControl{
+	private float delta;
+	private float speed;
+	private float offsetX = 0;
+	private float offsetY = 0;
+	private float kafSlowly = 0.4f;
+	
+	public MobileControl() {
+		this.speed = GameContext.getInstance().speed;
+	}
+	
 	@Override
 	public float getOffsetX() {
-		// TODO Auto-generated method stub
-		return 0;
+		return offsetX;
 	}
 
 	@Override
 	public float getOffsetY() {
-		// TODO Auto-generated method stub
-		return 0;
+		return offsetY;
 	}
 
 	@Override
 	public void update(float time) {
-		// TODO Auto-generated method stub
 		
+		float mX = Gdx.input.getAccelerometerY()* GameContext.getInstance().speed*time*kafSlowly;
+		float mY = Gdx.input.getAccelerometerX()* GameContext.getInstance().speed*time*kafSlowly;
+		
+		//Attention! Directions of coordinate axis in land position of phone don't coincides
+		offsetX = mX;
+		offsetY = -mY;	
 	}
 
 }
